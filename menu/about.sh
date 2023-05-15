b8155cb9c1d1ee5cfe987137096b2cbdb28e6ca2
givpn <givpn11@gmail.com>
2023-05-15 09:58:14 +0700

    x

---
+++ menu/about.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+clear
+echo -e "\e[1;31m    :::::::: ::::::::::: :::     ::: :::::::::  ::::    :::   \e[m"
+echo -e "\e[1;31m   :+:    :+:    :+:     :+:     :+: :+:    :+: :+:+:   :+:   \e[m"
+echo -e "\e[1;31m   +:+           +:+     +:+     +:+ +:+    +:+ :+:+:+  +:+   \e[m"
+echo -e "\e[1;31m   :#:           +#+     +#+     +:+ +#++:++#+  +#+ +:+ +#+   \e[m"
+echo -e "\e[1;31m   +#+   +#+#    +#+      +#+   +#+  +#+        +#+  +#+#+#   \e[m"
+echo -e "\e[1;31m   #+#    #+#    #+#       #+#+#+#   #+#        #+#   #+#+#   \e[m"
+echo -e "\e[1;31m    ######## ###########     ###     ###        ###    ####   \e[m"
+echo -e "\e[1;31m                        telegram @givpn                       \e[m"
+echo -e "\e[1;31m                   telegram grup @givpn_grup                  \e[m"
+echo ""
+echo ""
+read -n 1 -s -r -p "Enter Untuk Kembali Ke Menu Utama"
+menu
